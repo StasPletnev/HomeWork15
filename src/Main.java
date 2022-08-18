f071d53cacd300d0cb1a262a3d2bf98bc8cb95ff
Stas <www.stas011@mail.ru>
2022-08-18 16:39:49 +0300

    Added stat

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SalesManager Manager1 = new SalesManager(new int[]{100, 20, 30, 50});
         System.out.println(Manager1.max());
+        System.out.println(Manager1.average());
 
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,21 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int average() {
+        int max = -1;
+        int min = 1_000_000;
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+
+            }
+            sum += sale;
+        }
+        return (sum - max - min) / sales.length;
+    }
 }
