741fb6cdb956c200db3782ab5227ed82116f0109
StasPletnev <49528785+StasPletnev@users.noreply.github.com>
2022-08-18 16:12:40 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager Manager1 = new SalesManager(new int[]{100, 20, 30, 50});
+        System.out.println(Manager1.max());
+
+    }
+}
